e89ddd0c525b6e1ff1e0b9a06a1ef66cbe41d311
Sam Le <samhuynhle@gmail.com>
2021-08-05 23:46:00 -0700

    spread example

---
+++ spread_example_combine_arrays.js
@@ -0,0 +1,12 @@
+var x  = [1,2,3,4,5]
+var y = [6,7,8,9,10]
+var merged = [x,y]
+console.log(merged)
+
+console.log()
+console.log()
+
+var x = [1,2,3,4,5]
+var y = [6,7,8,9,10]
+var z =[...x, ...y];
+console.log(z)
